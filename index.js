34fbb4894aa6337a0e67505e621f31e41963b7bb
Sam M <sammontalvojr@gmail.com>
2021-01-21 13:18:04 -0600

    adding introspection and playground

--- index.js
+++ index.js
@@ -76,10 +76,17 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen({
-    port: process.env.PORT || 4000
-}).then(({ url }) => {
-  console.log(`Server started at ${url}`);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
 });
+
+server
+  .listen({
+    port: process.env.PORT || 4000,
+  })
+  .then(({ url }) => {
+    console.log(`Server started at ${url}`);
+  });
